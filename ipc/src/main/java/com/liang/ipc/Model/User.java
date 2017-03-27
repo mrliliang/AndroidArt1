6f1cdb4f9e3ba953ae363f9f8ab44645f4134757
liliang <630425651@qq.com>
2017-03-27 09:08:14 +0800

    IPC。

--- ipc/src/main/java/com/liang/ipc/Model/User.java
+++ ipc/src/main/java/com/liang/ipc/Model/User.java
@@ -3,6 +3,8 @@ package com.liang.ipc.Model;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import com.liang.ipc.aidl.Book;
+
 import java.io.Serializable;
 
 /**
@@ -10,26 +12,51 @@ import java.io.Serializable;
  */
 
 public class User implements Parcelable, Serializable {
+
+    private static final long serialVersionUID = 0;
+
+    public int userId;
+    public String userName;
+    public boolean isMale;
+
+    public Book book;
+
+    public User(int userId, String userName, boolean isMale) {
+        this.userId = userId;
+        this.userName = userName;
+        this.isMale = isMale;
+    }
+
     @Override
     public int describeContents() {
         return 0;
     }
 
     @Override
-    public void writeToParcel(Parcel dest, int flags) {
-
+    public void writeToParcel(Parcel out, int flags) {
+        out.writeInt(userId);
+        out.writeString(userName);
+        out.writeInt(isMale ? 1 : 0);
+        out.writeParcelable(book, 0);
     }
 
     public static final Parcelable.Creator<User> CREATOR = new Parcelable.Creator<User>() {
 
         @Override
-        public User createFromParcel(Parcel source) {
-            return null;
+        public User createFromParcel(Parcel in) {
+            return new User(in);
         }
 
         @Override
         public User[] newArray(int size) {
-            return new User[0];
+            return new User[size];
         }
     };
+
+    private User(Parcel in) {
+        userId = in.readInt();
+        userName = in.readString();
+        isMale = in.readInt() == 1;
+        book = in.readParcelable(Thread.currentThread().getContextClassLoader());
+    }
 }
