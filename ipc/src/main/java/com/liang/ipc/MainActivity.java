ff92981108e1de141d10d364368bbeb79f2acf9a
liliang <630425651@qq.com>
2017-04-14 09:07:28 +0800

    IPC。

--- ipc/src/main/java/com/liang/ipc/MainActivity.java
+++ ipc/src/main/java/com/liang/ipc/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 import com.liang.ipc.manager.UserManager;
 import com.liang.ipc.model.User;
