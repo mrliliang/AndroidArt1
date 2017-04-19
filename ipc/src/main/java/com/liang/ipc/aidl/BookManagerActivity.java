0d6c73d80287c7dc45ab76f41fb460145fd2ce1b
liliang <630425651@qq.com>
2017-04-19 13:44:06 +0800

    修改AIDL IPC。

--- ipc/src/main/java/com/liang/ipc/aidl/BookManagerActivity.java
+++ ipc/src/main/java/com/liang/ipc/aidl/BookManagerActivity.java
@@ -4,7 +4,9 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,20 +19,39 @@ import java.util.List;
 public class BookManagerActivity extends AppCompatActivity {
 
     private static final String TAG = "BookManagerActivity";
+    private static final int MESSAGE_NEW_BOOK_ARRIVED = 1;
+
+    private IBookManager mRemoteBookManager;
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MESSAGE_NEW_BOOK_ARRIVED:
+                    Log.i(TAG, "receive new book:" + msg.obj);
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    };
 
     private ServiceConnection mConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
             IBookManager bookManager = IBookManager.Stub.asInterface(service);
             try {
+                mRemoteBookManager = bookManager;
                 List<Book> list = bookManager.getBookList();
                 Log.i(TAG, "query book list, list type:" + list.getClass().getCanonicalName());
                 Log.i(TAG, "query book list:" + list.toString());
 
-                Book newBook = new Book(3, "Android开发艺术探索");
+                Book newBook = new Book(3, "Android进阶");
                 bookManager.addBook(newBook);
+                Log.i(TAG, "add book:" + newBook);
                 List<Book> newList = bookManager.getBookList();
                 Log.i(TAG, "query book list:" + newList.toString());
+                bookManager.registerListener(mOnNewBookArrivedListener);
             } catch (RemoteException e) {
                 e.printStackTrace();
             }
@@ -38,7 +59,15 @@ public class BookManagerActivity extends AppCompatActivity {
 
         @Override
         public void onServiceDisconnected(ComponentName name) {
+            mRemoteBookManager = null;
+            Log.e(TAG, "binder died.");
+        }
+    };
 
+    private IOnNewBookArrivedListener mOnNewBookArrivedListener = new IOnNewBookArrivedListener.Stub() {
+        @Override
+        public void onNewBookArrived(Book newBook) throws RemoteException {
+            mHandler.obtainMessage(MESSAGE_NEW_BOOK_ARRIVED, newBook).sendToTarget();
         }
     };
 
@@ -52,6 +81,14 @@ public class BookManagerActivity extends AppCompatActivity {
 
     @Override
     protected void onDestroy() {
+        if (mRemoteBookManager != null && mRemoteBookManager.asBinder().isBinderAlive()) {
+            try {
+                Log.i(TAG, "unregister listener:" + mOnNewBookArrivedListener);
+                mRemoteBookManager.unregisterListener(mOnNewBookArrivedListener);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
         unbindService(mConnection);
         super.onDestroy();
     }
--- ipc/src/main/java/com/liang/ipc/aidl/BookManagerService.java
+++ ipc/src/main/java/com/liang/ipc/aidl/BookManagerService.java
@@ -4,16 +4,26 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
+import android.os.RemoteCallbackList;
 import android.os.RemoteException;
+import android.util.Log;
 
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class BookManagerService extends Service {
 
     private static final String TAG = "BMS";
 
+    private AtomicBoolean mIsServiceDestroyed = new AtomicBoolean(false);
+
     private CopyOnWriteArrayList<Book> mBookList = new CopyOnWriteArrayList<Book>();
+
+//    private CopyOnWriteArrayList<IOnNewBookArrivedListener> mListenerList =
+//            new CopyOnWriteArrayList<IOnNewBookArrivedListener>();
+    private RemoteCallbackList<IOnNewBookArrivedListener> mListenerList =
+            new RemoteCallbackList<IOnNewBookArrivedListener>();
 
     private Binder mBinder = new IBookManager.Stub() {
 
@@ -29,12 +39,26 @@ public class BookManagerService extends Service {
 
         @Override
         public void registerListener(IOnNewBookArrivedListener listener) throws RemoteException {
-
+//            if (!mListenerList.contains(listener)) {
+//                mListenerList.add(listener);
+//            } else {
+//                Log.i(TAG, "already exists.");
+//            }
+            mListenerList.register(listener);
+            Log.i(TAG, "registerListener, size:" + mListenerList.getRegisteredCallbackCount());
         }
 
         @Override
         public void unregisterListener(IOnNewBookArrivedListener listener) throws RemoteException {
-
+//            if (mListenerList.contains(listener)) {
+//                mListenerList.remove(listener);
+//                Log.i(TAG, "unregister listener succeed.");
+//            } else {
+//                Log.i(TAG, "not found, can not unregister.");
+//            }
+            mListenerList.unregister(listener);
+            Log.i(TAG, "unregisterListener, current size:" +
+                    mListenerList.getRegisteredCallbackCount());
         }
 
 
@@ -50,5 +74,56 @@ public class BookManagerService extends Service {
         super.onCreate();
         mBookList.add(new Book(1, "Android"));
         mBookList.add(new Book(2, "IOS"));
+        new Thread(new ServiceWorker()).start();
     }
+
+    @Override
+    public void onDestroy() {
+        mIsServiceDestroyed.set(true);
+        super.onDestroy();
+    }
+
+    private void onNewBookArrived(Book book) throws RemoteException {
+        mBookList.add(book);
+//        Log.i(TAG, "onNewBookArrived, notify listeners:" + mListenerList.size());
+//        for (int i = 0; i < mListenerList.size(); i++) {
+//            IOnNewBookArrivedListener listener = mListenerList.get(i);
+//            Log.i(TAG, "onNewBookArrived, notify listener:" + listener);
+//            listener.onNewBookArrived(book);
+//        }
+        final int N = mListenerList.beginBroadcast();
+        for (int i = 0; i < N; i++) {
+            IOnNewBookArrivedListener l = mListenerList.getBroadcastItem(i);
+            if (l != null) {
+                try {
+                    l.onNewBookArrived(book);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        mListenerList.finishBroadcast();
+    }
+
+    private class ServiceWorker implements Runnable {
+        @Override
+        public void run() {
+            while (!mIsServiceDestroyed.get()) {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                int bookId = mBookList.size() + 1;
+                Book newBook = new Book(bookId, "new book#" + bookId);
+                try {
+                    onNewBookArrived(newBook);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    ;
 }
