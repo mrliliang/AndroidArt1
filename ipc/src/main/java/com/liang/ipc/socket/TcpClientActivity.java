0dcda069fa2f24ae77789fc9e0039056ae398ed9
liliang <630425651@qq.com>
2017-04-27 09:20:57 +0800

    IPC Socket。

--- ipc/src/main/java/com/liang/ipc/socket/TcpClientActivity.java
+++ ipc/src/main/java/com/liang/ipc/socket/TcpClientActivity.java
@@ -1,20 +1,33 @@
 package com.liang.ipc.socket;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.Message;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.liang.ipc.R;
+import com.liang.ipc.utils.MyUtils;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
 
-public class TcpClientActivity extends AppCompatActivity {
+public class TcpClientActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final int MESSAGE_RECEIVE_NEW_MSG = 1;
     private static final int MESSAGE_SOCKET_CONNNECTED = 2;
@@ -30,13 +43,120 @@ public class TcpClientActivity extends AppCompatActivity {
     private Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
-            super.handleMessage(msg);
+            switch (msg.what) {
+                case MESSAGE_RECEIVE_NEW_MSG:
+                    mMessageEditText.append((String)msg.obj);
+                    break;
+                case MESSAGE_SOCKET_CONNNECTED:
+                    mSendButton.setEnabled(true);
+                    break;
+                default:
+                    break;
+            }
         }
     };
+
+    private HandlerThread mSendMsgThread = new HandlerThread("send_message_thread");
+    private Handler mSendMsgHandler;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tcp_client);
+        mMessageTextView = (TextView) findViewById(R.id.msg_container);
+        mSendButton = (Button) findViewById(R.id.send);
+        mSendButton.setOnClickListener(this);
+        mMessageEditText = (EditText) findViewById(R.id.msg);
+
+        mSendMsgThread.start();
+        mSendMsgHandler = new Handler(mSendMsgThread.getLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                if (mPrintWriter != null) {
+                    mPrintWriter.println((String)msg.obj);
+                }
+            }
+        };
+        Intent service = new Intent(this, TCPServerService.class);
+        startService(service);
+        new Thread() {
+            @Override
+            public void run() {
+                connectTCPServer();
+            }
+        }.start();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mSendButton) {
+            final String msg = mMessageEditText.getText().toString();
+            if (!TextUtils.isEmpty(msg) && mPrintWriter != null) {
+//                mPrintWriter.println(msg);
+                mSendMsgHandler.obtainMessage(0, msg).sendToTarget();
+                mMessageEditText.setText("");
+                String time = formatDateTime(System.currentTimeMillis());
+                final String showedMsg = "self " + time + ":" + msg + "\n";
+                mMessageTextView.setText(mMessageTextView.getText() + showedMsg);
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mClientSocket != null) {
+            try {
+                mClientSocket.shutdownInput();
+                mClientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        mSendMsgThread.quit();
+        super.onDestroy();
+    }
+
+    private String formatDateTime(long time) {
+        return new SimpleDateFormat("(HH:mm:ss)").format(time);
+    }
+
+    private void connectTCPServer() {
+        Socket socket = null;
+        while (socket == null) {
+            try {
+                socket = new Socket("localhost", 8688);
+                mClientSocket = socket;
+                mPrintWriter = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket
+                        .getOutputStream())), true);
+                mHandler.sendEmptyMessage(MESSAGE_SOCKET_CONNNECTED);
+                System.out.println("Connect server success.");
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                SystemClock.sleep(1000);
+                e.printStackTrace();
+                System.out.println("connect tcp server failed, retry...");
+            }
+        }
+
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            while (!TcpClientActivity.this.isFinishing()) {
+                String msg = br.readLine();
+                System.out.println("receive : " + msg);
+                if (msg != null) {
+                    String time = formatDateTime(System.currentTimeMillis());
+                    final String showedMsg = "Server " + time + ":" + msg + "\n";
+                    mHandler.obtainMessage(MESSAGE_RECEIVE_NEW_MSG, showedMsg).sendToTarget();
+                }
+            }
+            System.out.println("quit...");
+            MyUtils.close(mPrintWriter);
+            MyUtils.close(br);
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
