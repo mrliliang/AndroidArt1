ffa48228c3ba990787a86563fd79043e7af49e23
liliang <630425651@qq.com>
2017-05-18 09:15:21 +0800

    View event animation.

--- viewevent/src/main/java/com/liang/viewevent/TestActivity.java
+++ viewevent/src/main/java/com/liang/viewevent/TestActivity.java
@@ -1,13 +1,82 @@
 package com.liang.viewevent;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class TestActivity extends AppCompatActivity {
+public class TestActivity extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener{
+
+    private static final String TAG = "TestActivity";
+
+    private static final int MESSAGE_SCROLL_TO = 1;
+    private static final int FRAME_COUNT = 30;
+    private static final int DELAY_TIME = 33;
+
+    private Button mButton1;
+    private View mButton2;
+
+    private int mCount = 0;
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MESSAGE_SCROLL_TO:
+                    mCount++;
+                    if (mCount <= FRAME_COUNT) {
+                        float fraction = mCount / (float) FRAME_COUNT;
+                        int scrollX = (int) (fraction * 100);
+                        mButton1.scrollTo(scrollX, 0);
+                        mHandler.sendEmptyMessageDelayed(MESSAGE_SCROLL_TO, DELAY_TIME);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        initView();
+    }
+
+    private void initView() {
+        mButton1 = (Button) findViewById(R.id.button1);
+        mButton1.setOnClickListener(this);
+        mButton2 = (TextView) findViewById(R.id.button2);
+        mButton2.setOnLongClickListener(this);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            Log.d(TAG, "button1.left　=　" + mButton1.getLeft());
+            Log.d(TAG, "button1.x = " + mButton1.getX());
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        return false;
     }
 }
