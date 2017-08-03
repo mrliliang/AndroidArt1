c4aca80ededab440bcc291caf093a4d97178ce7e
liliang <630425651@qq.com>
2017-08-03 10:09:06 +0800

    Memory Leak.

--- performanceimprove/src/main/java/com/example/performanceimprove/MainActivity.java
+++ performanceimprove/src/main/java/com/example/performanceimprove/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.performanceimprove;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static Context sContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sContext = this;
     }
 }
